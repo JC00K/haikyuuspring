99b01e6bea35ffc73fe28e249e0112fc65850eb3
Jeremy Cook <Jcook61592@gmail.com>
2026-01-30 15:46:29 -0500

    Adjusted CoachesStyleEnum order to refelct proper default non coach value

--- src/main/java/com/example/haikyuuspring/model/enums/CoachingStyle.java
+++ src/main/java/com/example/haikyuuspring/model/enums/CoachingStyle.java
@@ -1,10 +1,10 @@
 package com.example.haikyuuspring.model.enums;
 
 public enum CoachingStyle {
+    NONCOACH,
     DEFENSE,
     ATTACK,
     BLOCK,
     AGGRESSIVE,
-    POWER,
-    NONCOACH
+    POWER
 }
