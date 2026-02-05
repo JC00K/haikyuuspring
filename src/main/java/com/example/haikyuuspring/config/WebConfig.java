ce7a8a871a37487b20e1c3d8ca72fcb7122a6230
Jeremy Cook <Jcook61592@gmail.com>
2026-02-05 14:06:18 -0500

    Updated web config

--- src/main/java/com/example/haikyuuspring/config/WebConfig.java
+++ src/main/java/com/example/haikyuuspring/config/WebConfig.java
@@ -1,7 +1,5 @@
 package com.example.haikyuuspring.config;
 
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,13 +7,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Value("${cors.allowed.origins}")
-    private String[] allowedOrigins;
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins(allowedOrigins)
+                .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
